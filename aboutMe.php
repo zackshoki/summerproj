b11e6743f1562534284a3f4e57cf92d618e797e0
“Zack <zackshoki@gmail.com>
2025-05-23 10:39:58 -0500

    files added

---
+++ aboutMe.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+    <head>
+        <meta charset="utf-8">
+        <title>ABOUT ZACK</title>
+        <style></style>
+    </head>
+    <body>
+        <h1>who is he?</h1>
+        <p>a robust computer engineer is in the making. watch out </p>
+        <img href="">
+    </body>
