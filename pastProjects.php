ee3412185c110d5ecbed6b50664d98c59d05d5a4
“Zack <zackshoki@gmail.com>
2025-05-27 17:02:07 -0500

    ughhhhhh

--- pastProjects.php
+++ pastProjects.php
@@ -7,18 +7,20 @@
 
    echo '
    <h1>PROJECTS<h1>
-   ';
+   <div class="projectContainer">';
+
 //    if(isset($_REQUEST['title'])){
     $allProjects = getAllProjects();
         foreach ($allProjects as $project) {
                 $title = $project['title'];
                 $projectId = $project['projectId'];
                 echo "
-                <div class='projectContainer'>
+                <div class='projectBlock'>
                     <h2><a href='view_project.php?projectId=".$projectId."'>".$title."</a><h2>
                 </div>
                 ";
         }
+        echo "</div>"
 
 //    } else {
 //     echo "no post is available.";
