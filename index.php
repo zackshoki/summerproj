b322d4150ecd77e3902029dc3f7511a4f65a4d6c
“Zack <zackshoki@gmail.com>
2025-06-09 15:12:58 -0500

    tried to do first curl request

--- index.php
+++ index.php
@@ -6,11 +6,11 @@
 <html>
     <head>
         <meta charset="utf-8" name="viewport" content="width=device-width">
-        <title>summerproj</title>
+        <title>summ3erproj</title>
         <link rel="stylesheet" href="stylesheets/styles.css">
-        <script type="text/javascript" src="scripts/main.js"></script>
+        <script defer type="text/javascript" src="scripts/main.js"></script>
     </head>
     <body>
-        <?php debugOutput($allUsers); ?> hello world
+        <div id="thing">hello</div>
     </body>
 </html>
