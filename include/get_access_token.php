f47e64b0884620109873c96c4c3268eef7858447
“Zack <zackshoki@gmail.com>
2025-06-11 11:34:00 -0500

    first step of user auth took way too long

--- include/get_access_token.php
+++ include/get_access_token.php
@@ -1,5 +1,5 @@
 <?php
-function getToken()
+function getToken() // no user authorization, uses client credentials
 {
     $client_id = 'e222be8a405045a79868de716b5aef44';
     $client_secret = 'd48a28f221264ee8bfe3ad63c33006be';
@@ -22,5 +22,16 @@ function getToken()
     $token_array = json_decode($recieved_data, true);
     $token = $token_array['access_token'];
     return $token;
+}
+function requestUserAuthorization() { // for user authorization
+// scopes needed: ugc-image-upload, user-modify-playback-state, playlist-read-private, playlist-read-collaborative, playlist-modify-private, playlist-modify-public, user-top-read, user-read-recently-played, user-library-modify, user-library-read, user-read-email, user-read-private, user-soa-link, soa-manage-partner,  
+// idk what the state parameter should be??
+    $client_id = 'e222be8a405045a79868de716b5aef44'; // probably should be stored in a different way? maybe should be a parameter in this function
+    $redirect_uri = 'http://[::1]:8888/index.php';
+    $scopes = 'ugc-image-upload%20user-modify-playback-state%20playlist-read-private%20playlist-read-collaborative%20playlist-modify-private%20playlist-modify-public%20user-top-read%20user-read-recently-played%20user-library-modify%20user-library-read%20user-read-email%20user-read-private';
+    $state = bin2hex(random_bytes(16 / 2));
+    $url = "https://accounts.spotify.com/authorize?client_id=$client_id&response_type=code&redirect_uri=$redirect_uri&scope=$scopes&show_dialog=true&state=$state";
+
+    header("Location: $url");
 }
 ?>
--- index.php
+++ index.php
@@ -1,25 +1,30 @@
-<?php 
-    include('include/init.php');
-    $allUsers = getAllUsers();
+<?php
+include('include/init.php');
+$allUsers = getAllUsers();
 ?>
 
 <html>
-    <head>
-        <meta charset="utf-8" name="viewport" content="width=device-width">
-        <title>summ3erproj</title>
-        <link rel="stylesheet" href="stylesheets/styles.css">
-        <script defer type="text/javascript" src="scripts/main.js"></script>
-    </head>
-    <body>
-        <div id="thing">
+
+<head>
+    <meta charset="utf-8" name="viewport" content="width=device-width">
+    <title>summ3erproj</title>
+    <link rel="stylesheet" href="stylesheets/styles.css">
+    <script defer type="text/javascript" src="scripts/main.js"></script>
+</head>
+
+<body>
+    <div id="thing">
         <h1>
-        <?php
-            $token = getToken();
-            $json_data = spotifyCurlRequest($token, "https://api.spotify.com/v1/artists/6vWDO969PvNqNYHIOW5v0m?si=UQO5ex8yS-WnjRHjV3R9Aw");
-            $data = json_decode($json_data, TRUE);
-            echo $data['name'];
-        ?> 
+            <?php
+            // $token = getToken();
+            // $json_data = spotifyCurlRequest($token, "https://api.spotify.com/v1/artists/6vWDO969PvNqNYHIOW5v0m?si=UQO5ex8yS-WnjRHjV3R9Aw");
+            // $data = json_decode($json_data, TRUE);
+            // echo $data['name'];
+
+            requestUserAuthorization();
+            ?>
         </h1>
-        </div>
-    </body>
+    </div>
+</body>
+
 </html>
