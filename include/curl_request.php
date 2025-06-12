c71e62a5d14d2b12688dac5d46bff32da3961d8e
“Zack <zackshoki@gmail.com>
2025-06-12 17:06:15 -0500

    trying to get all the apis to interact

--- include/curl_request.php
+++ include/curl_request.php
@@ -19,4 +19,45 @@ function spotifyGetRequest($token, $url, $formatted_fields)
     return $data; 
     
 }
+// acoustic brains/music brainz
+$musicURL = 'https://musicbrainz.org/ws/2/';
+$acousticURL = 'https://acousticbrainz.org/api/v1/';
+
+function isrcToMBID($isrc) { // isrc is a universally identifying code for a specific song recording, mbid is this-api specific idenifying code for the same recording
+    global $musicURL; 
+    $curl = curl_init(); 
+
+    $curl_options = [
+        CURLOPT_URL => $musicURL."isrc/$isrc",
+        CURLOPT_HTTPHEADER => [
+            'Content-Type: application/json',
+            'User-Agent: Sprintify/1.0 ( zackshoki@gmail.com )'
+        ],
+        CURLOPT_CUSTOMREQUEST => "GET",
+        CURLOPT_RETURNTRANSFER => TRUE
+    ];
+    curl_setopt_array($curl, $curl_options);
+    $mbid =  curl_exec($curl);
+    echo $mbid; //this is not an mbid. what is it?
+    // return $mbid;
+}
+function fetchTrackData($mbids) {
+    global $acousticURL;
+    $curl = curl_init(); 
+
+    $curl_options = [
+        CURLOPT_URL => $acousticURL."low-level?map_classes='true'&recording_ids=".$mbids,
+        CURLOPT_HTTPHEADER => [
+            'Content-Type: application/json'
+        ],
+        CURLOPT_CUSTOMREQUEST => "GET",
+        CURLOPT_RETURNTRANSFER => TRUE
+    ];
+    curl_setopt_array($curl, $curl_options);
+    $data_json = curl_exec($curl);
+    echo $data_json;
+    $data = json_decode($data_json, true);
+
+    return $data;
+}
 ?>
--- index.php
+++ index.php
@@ -1,18 +1,18 @@
 <?php
 include('include/init.php');
 $allUsers = getAllUsers();
-if (!isset($_COOKIE['spotify_token'])) {
-    if (isset($_REQUEST['code']) && isset($_REQUEST['state'])) {
+// if (!isset($_COOKIE['spotify_token'])) {
+//     if (isset($_REQUEST['code']) && isset($_REQUEST['state'])) {
         
-        $token = requestAccessToken($_REQUEST['code'], $_REQUEST['state']); 
-    } else {
-        header('Location: login.php'); // work on refresh tokens in the future. 
-    }
+//         $token = requestAccessToken($_REQUEST['code'], $_REQUEST['state']); 
+//     } else {
+//         header('Location: login.php'); // work on refresh tokens in the future. 
+//     }
     
-} else {
-    $token = $_COOKIE['spotify_token'];
-}
-?>
+// } else {
+//     $token = $_COOKIE['spotify_token'];
+// }
+// ?>
 
 <html>
 
@@ -21,11 +21,16 @@ if (!isset($_COOKIE['spotify_token'])) {
     <title>summ3erproj</title>
     <link rel="stylesheet" href="stylesheets/styles.css">
     <script defer type="text/javascript" src="scripts/main.js">
-        populateUI(fetchProfile(<?php echo $token ?>));
+       // populateUI(fetchProfile(</?php echo $token ?>));
+       
     </script>
 </head>
 
 <body>
+    <?php 
+    // $data = fetchTrackData(isrctoMBID('USSM11603181'));
+    // var_dump( $data);
+    ?>
     <div id="avatar"></div>
     <div id="displayName"></div>
     <div id="imgUrl"></div>
